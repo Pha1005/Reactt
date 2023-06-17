b6a1c51293f7a3a90db17f0d38895a117bd2bc4f
Pha1005 <anhpham010102@gmail.com>
2023-06-17 09:04:10 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -7,27 +7,19 @@ import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 
 
-  class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        dishes: DISHES
-      };
-    }
-  
+import Main from './components/MainComponent';
+
+class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
 }
+
   
 
 
